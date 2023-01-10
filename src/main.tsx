13258eaa46bd93e49d01a0c6ff388dd2f4e3077f
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2023-01-10 18:10:22 +0100

    feat: create router template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import App from "./App";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+} from "react-router-dom";
+import Auth from "./pages/Auth";
+import DashboardLayout from "./pages/DashboardLayout";
+import AddBookForm from "./components/AddBookForm";
+import Library from "./components/Library";
+import Dashboard from "./components/Dashboard";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "auth",
+    element: <Auth />,
+  },
+  // protected routes:
+  {
+    path: "dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        loader() {
+          return redirect("/dashboard/main");
+        },
+      },
+      { path: "library", element: <Library /> },
+      { path: "main", element: <Dashboard /> },
+    ],
+  },
+  {
+    path: "add",
+    element: <AddBookForm />,
+  },
+]);
+
+createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
