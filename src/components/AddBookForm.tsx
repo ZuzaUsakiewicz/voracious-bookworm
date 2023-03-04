6d3a6515f2272ecbb6dc6648e6b6d4e623df0584
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2023-03-04 20:51:35 +0100

    feat: add template routes

---
+++ src/components/AddBookForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddBookForm() {
+  return (
+    <div>
+      <h1>Add Book</h1>
+    </div>
+  );
+}
+
+export default AddBookForm;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+  return (
+    <div>
+      <Link to="/dashboard/main">login</Link>
+    </div>
+  );
+}
+
+export default Login;
